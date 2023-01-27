44c4e8c8b393719d0412cd088d355d4aa62499a8
titkoolivermilan <113435740+titkoolivermilan@users.noreply.github.com>
2023-01-27 12:06:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a=int(input("Kerem az elso szamot:"))
+b=int(input("Kerem a masodik szamot:"))
+
+kisebb=min(a,b)
+nagyobb=max(a,b)
+if (kisebb!=nagyobb):
+    print("kisebb szam:", kisebb)
+    print("nagyobb szam:", nagyobb)
